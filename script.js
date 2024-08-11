f56013b565be633f051df0e3afd6de9550fb4531
Roman Chernetsky <misteryo.wins@gmail.com>
2024-08-11 19:40:13 +0300

    new sidebar. it has a lot of shitty code and bad practice (didnt want to make it right)

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-<script>
-  new WOW().init();
-</script>
+let currentOpenMenu = document.getElementById("home"); // Home menu is always open initially
+
+function toggleMenu(menuId) {
+  const menuContent = document.getElementById(menuId);
+
+  if (currentOpenMenu && currentOpenMenu !== menuContent) {
+    currentOpenMenu.classList.remove("expanded");
+  }
+
+  if (menuContent.classList.contains("expanded")) {
+    // If the clicked menu is already expanded, do nothing
+    return;
+  } else {
+    menuContent.classList.add("expanded");
+  }
+
+  currentOpenMenu = menuContent;
+}
